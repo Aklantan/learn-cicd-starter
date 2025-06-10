a84ede00b6811f71b9cdd0c363820754f54210bd
Aklantan <allan.tucker@gmail.com>
2025-06-10 22:06:23 +0100

    added lint tests

--- main.go
+++ main.go
@@ -22,6 +22,11 @@ type apiConfig struct {
 	DB *database.Queries
 }
 
+func unused() {
+	// this function does nothing
+	// and is called nowhere
+}
+
 //go:embed static/*
 var staticFiles embed.FS
 
